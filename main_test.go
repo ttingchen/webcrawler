915844a3ace31c92ded29658e9298285829aade8
Kyle Chen <kyle.chen@cienet.com>
2021-08-03 14:42:12 +0800

    unit test ebay

--- main_test.go
+++ main_test.go
@@ -31,3 +31,23 @@ func Test_collectWatsons(t *testing.T) {
 		})
 	}
 }
+
+func Test_collectEbay(t *testing.T) {
+	type args struct {
+		search_item string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := collectEbay(tt.args.search_item); (err != nil) != tt.wantErr {
+				t.Errorf("collectEbay() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
